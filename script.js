77aeb7c5246528045f8f6254ec95dd303246caf0
Daniel Kim <78152949+dannykim1997@users.noreply.github.com>
2023-09-06 13:17:02 -0700

    Create basic grid layout of cards

--- script.js
+++ script.js
@@ -3,51 +3,61 @@ const myLibrary = [
         title: "The Hobbit",
         author: "J.R.R. Tolkien",
         pages: 295,
-        // read: false
+        read: false
     },
     {
         title: "Naruto",
         author: "Weeb",
         pages: 100,
-        // read: false
+        read: false
     }
 ];
 
-const bookCardsContainer = document.querySelector(".book-cards");
+const bookCardsContainer = document.querySelector(".book-cards-container");
 
-myLibrary.forEach((book) => {
-    let bookCard = document.createElement("div");
-    let bookTitle = document.createElement("div");
-    let bookAuthor = document.createElement("div");
-    let bookPages = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookTitle.classList.add("book-title");
-    bookAuthor.classList.add("book-author");
-    bookPages.classList.add("book-pages");
-    bookTitle.textContent = book.title;
-    bookAuthor.textContent = book.author;
-    bookPages.textContent = book.pages;
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(bookPages);
-    bookCardsContainer.appendChild(bookCard);
-})
-
-// function Book(title, author, pages) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     // this.read = read;
-// }
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
 
-// function addBookToLibrary() {
-//     let title = bookTitle.textContent;
-//     let author = bookAuthor.textContent;
-//     let pages = bookPages.textContent;
-//     let newBook = new Book(title, author, pages);
-//     myLibrary.push(newBook);
-// }
-// addBookToLibrary()
+function addBookToLibrary() {
+    let title = "Hairy Pothead";
+    let author = "JJK Rolling";
+    let pages = 293;
+    let read = false;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks()
+}
+addBookToLibrary()
 
-
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        let bookCard = document.createElement("div");
+        let bookTitle = document.createElement("div");
+        let bookAuthor = document.createElement("div");
+        let bookPages = document.createElement("div");
+        let bookRead = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookTitle.classList.add("book-title");
+        bookAuthor.classList.add("book-author");
+        bookPages.classList.add("book-pages");
+        bookRead.classList.add("book-read");
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        if(book.read) {
+            bookRead.textContent = "read"
+        } else {
+            bookRead.textContent = "not read"
+        }
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+        bookCardsContainer.appendChild(bookCard);
+    })
+}
